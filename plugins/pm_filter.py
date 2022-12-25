ef992c3e814a914cb639920239a722ffe1434524
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-12-25 21:42:39 +0530

    Try

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -366,10 +366,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                           reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('🔰𝗛ᴏᴡ 𝗧ᴏ 𝗗ᴏᴡɴʟᴏᴀᴅ 🔰', url=H_DOWNLOAD_LINK)
+                    InlineKeyboardButton('⚡ Telegram File Link - Dulink ⚡', url=g)
                 ],
                 [
-                    InlineKeyboardButton('♻️ 𝗗ᴏᴡɴʟᴏᴀᴅ 𝗟ɪɴᴋ ♻️', url=g)
+                    InlineKeyboardButton('How To Get Telegram File ⁉️', url=H_DOWNLOAD_LINK)
                 ]]))
                 await query.answer('Check PM, I have sent files in pm', show_alert=True)
         except UserIsBlocked:
