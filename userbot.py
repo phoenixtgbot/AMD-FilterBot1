8bcb0512180a3522f6fa819ef3d866170f2c54e8
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2023-02-19 21:34:21 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,8 +1,7 @@
 from info import *
 from pyrogram import Client, idle
 
-User = Client(session_name="userbot",
-              session_string=DEL_SESSION,
+User = Client(session_string=DEL_SESSION,
               api_id=API_ID,
               api_hash=API_HASH,
               workers=300
