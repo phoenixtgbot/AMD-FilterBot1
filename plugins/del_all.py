f16869dfc1c8eeed094ac6879af238d751aa77a1
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2023-02-19 20:33:11 +0530

    Create del_all.py

---
+++ plugins/del_all.py
@@ -0,0 +1,16 @@
+# This Model Added By @ThiruXD
+
+import asyncio
+from info import *
+from pyrogram import Client, filters, idle
+
+@User.on_message(filters.chat(GROUPS))
+async def delete(user, message):
+    try:
+       if message.from_user.id in ADMINS:
+          return
+       else:
+          await asyncio.sleep(DEL_TIME)
+          await Bot.delete_messages(message.chat.id, message.id)
+    except Exception as e:
+       print(e)
